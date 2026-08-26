d904e7680089b6a2ede86f8db508112a367ca00d
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-08-26 22:35:32 +0300

    chore: initialize Spring Boot project with core dependencies

---
+++ src/main/java/dev/arnel/scholartrackapi/ScholarTrackApiApplication.java
@@ -0,0 +1,13 @@
+package dev.arnel.scholartrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarTrackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/arnel/scholartrackapi/ScholarTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arnel.scholartrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarTrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
